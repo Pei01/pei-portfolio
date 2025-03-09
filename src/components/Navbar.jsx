1afb724ca011d8f2c3e0e3dc8777d2f37fe433e6
Pei Wu <starwu02@gmail.com>
2025-03-10 00:06:14 +0800

    feat: navbar element hover underline animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,8 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
         document.body.style.overflow = menuOpen ? "hidden" : "";
     }, [menuOpen]);
 
+    const navItems = ["Home", "About", "Projects", "Contact"];
+
     return (
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10 ,10 ,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
             <div className="max-w-5xl mx-auto px-4">
@@ -20,30 +22,15 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
                     </div>
 
                     <div className="hidden md:flex items-center space-x-8">
-                        <a 
-                            href="#home" 
-                            className="text-gray-300 hove:text-white transition-colors"
-                        >
-                            Home
-                        </a>
-                        <a 
-                            href="#about" 
-                            className="text-gray-300 hove:text-white transition-colors"
-                        >
-                            About
-                        </a>
-                        <a 
-                            href="#projects" 
-                            className="text-gray-300 hove:text-white transition-colors"
-                        >
-                            Projects
-                        </a>
-                        <a 
-                            href="#contact" 
-                            className="text-gray-300 hove:text-white transition-colors"
-                        >
-                            Contact
-                        </a>
+                        {navItems.map((item, index) => (
+                            <a 
+                                key={index}
+                                href={`#${item.toLowerCase()}`}
+                                className="nav-hover-btn"
+                            >
+                                {item}
+                            </a>
+                        ))}
                     </div>
                 </div>
             </div>
